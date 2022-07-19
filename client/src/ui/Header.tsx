f8643ef3f1109c4b7e35aef64201458f8683a221
cyppan <cpannier@beop.io>
2022-07-19 09:34:37 +0200

    better title

--- client/src/ui/Header.tsx
+++ client/src/ui/Header.tsx
@@ -10,12 +10,16 @@ export default function Header({ left, right }: HeaderProps) {
   return (
     <div className="Header">
       <div className="Header-left">
-        <h1>XSMLX</h1>
+        <h1>
+          <span style={{ fontSize: '0.6em' }}>X</span>
+          <span style={{ fontSize: '0.7em' }}>S</span>
+          <span style={{ fontSize: '0.8em' }}>M</span>
+          <span style={{ fontSize: '0.9em' }}>L</span>
+          <span>X</span>
+        </h1>
         {left}
       </div>
-      <div className="Header-right">
-        {right}
-      </div>
+      <div className="Header-right">{right}</div>
     </div>
   );
 }
