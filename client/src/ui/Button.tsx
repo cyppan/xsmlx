489500e44f8d7714c33d973a9b39649c59611ce6
cyppan <cpannier@beop.io>
2022-07-22 11:31:50 +0200

    improve button

--- client/src/ui/Button.tsx
+++ client/src/ui/Button.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React from 'react';
 import './Button.css';
 
 interface ButtonProps extends React.PropsWithChildren {
-  disabled?: boolean,
-  handleClick: React.MouseEventHandler<HTMLButtonElement>
+  disabled?: boolean;
+  handleClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({children, handleClick, disabled}: ButtonProps) {
+export default function Button({
+  children,
+  handleClick,
+  disabled,
+}: ButtonProps) {
   return (
-    <button className="Button" onClick={handleClick} disabled={disabled}>{children}</button>
-  )
+    <button className="Button" onClick={handleClick} disabled={disabled}>
+      {children}
+    </button>
+  );
 }
